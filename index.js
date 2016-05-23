2b5368a348978ab16091f6fd6632a736563424d7
Philip Huysmans <philip.huysmans@gmail.com>
2016-05-23 20:51:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var phgms_core = require('../phgms_core');
+var mqtt_adapter = require('../mqtt_adapter');
+var phgms_sentinel = require('../phgms_sentinel');
+var logger = require('../logger');
+
+var gms = new phgms_core({
+	moment: require('moment')
+});
+
+var sentinel = new phgms_sentinel(
+	{
+		moment: require('moment'),
+		logger: logger
+	}, gms);
+
+var adapter = new mqtt_adapter(
+	{
+		mqtt: require('mqtt')
+	}, 
+	{
+		mqttHost: "mqtt://192.168.1.124",
+		topic: 'phgms/state',
+		callback: gms.handleMessage
+	}
+);
+
+var rest_api = require('../phgms_rest_api/app')(gms);
+rest_api.set('port', process.env.PORT || 3000);
+
+var server = rest_api.listen(rest_api.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
